2fc922fe38fbf7e02d2198c29cee4fc47389a998
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-09-14 00:24:55 +0900

    even when scroll, move circle smoothly

--- src/components/Cards/Content1.jsx
+++ src/components/Cards/Content1.jsx
@@ -6,46 +6,52 @@ import useMouse from "@react-hook/mouse-position";
 
 const Wrapper = styled(Card)`
   position: relative;
-  display: flex;
-  /* place-content: center;
-  place-items: center; */
   overflow: hidden;
   font-size: 1500px;
 `;
 const Circle = styled(motion.div)`
   position: absolute;
-  width: 200px;
-  height: 200px;
+  width: ${(props) => props.size}px; // Use the size prop to set the width
+  height: ${(props) => props.size}px; // Use the size prop to set the height
   border-radius: 50%;
   background-color: #9bf50b;
 `;
 
 function Content1() {
-  const [circlePosition, setCirclePosition] = useState({ x: 0, y: 0 });
+  const circleSize = 100; // Define the size of the circle
+  const halfCircle = circleSize / 2;
+  const [circlePosition, setCirclePosition] = useState({
+    x: halfCircle,
+    y: halfCircle,
+  });
   const [scrollY, setScrollY] = useState(0); // State to store the scrolled pixels
   const [wrapperPosition, setWrapperPosition] = useState({ left: 0, top: 0 }); // State to store wrapper position
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
+  const [mouseEntered, setMouseEntered] = useState(false);
   const constraintsRef = useRef(null);
 
+  const updateCirclePosition = (e) => {
+    const mouseX = e.clientX - wrapperPosition.left - halfCircle;
+    const mouseY = e.clientY - wrapperPosition.top - halfCircle;
+    setCirclePosition({ x: mouseX, y: mouseY });
+  };
+
+  const updateScrollY = () => {
+    setScrollY(window.scrollY);
+  };
+  const updateWindowWidth = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
   useEffect(() => {
-    const updateCirclePosition = (e) => {
-      const mouseX = e.clientX - wrapperPosition.left - 75;
-      const mouseY = e.clientY - wrapperPosition.top - 75;
-      setCirclePosition({ x: mouseX, y: mouseY });
-    };
-
-    const updateScrollY = () => {
-      setScrollY(window.scrollY);
-    };
-    const updateWindowWidth = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
     const rect = constraintsRef.current.getBoundingClientRect();
-    setWrapperPosition({ left: rect.left, top: rect.top });
+    const initialX = rect.width / 2 - halfCircle;
+    const initialY = rect.height / 2 - halfCircle;
 
-    document.addEventListener("mousemove", updateCirclePosition);
+    setCirclePosition({ x: initialX, y: initialY });
+    setWrapperPosition({ left: rect.left, top: rect.top });
+    mouseEntered &&
+      document.addEventListener("mousemove", updateCirclePosition);
     window.addEventListener("scroll", updateScrollY);
     window.addEventListener("resize", updateWindowWidth);
     return () => {
@@ -53,15 +59,24 @@ function Content1() {
       window.removeEventListener("scroll", updateScrollY);
       window.removeEventListener("resize", updateWindowWidth);
     };
-  }, [scrollY, windowWidth]);
+  }, [windowWidth, mouseEntered, scrollY]);
 
-  useEffect(() => {
-    console.log(windowWidth);
-  }, [windowWidth]);
+  const handleMouseEnter = () => {
+    setMouseEntered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setMouseEntered(false);
+  };
 
   return (
-    <Wrapper ref={constraintsRef}>
+    <Wrapper
+      ref={constraintsRef}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
       <Circle
+        size={circleSize}
         animate={{ x: circlePosition.x, y: circlePosition.y }}
         transition={{ type: "spring", stiffness: 500, damping: 100 }} // Adjust these values for desired animation
       />{" "}
