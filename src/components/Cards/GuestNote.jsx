fb5e836c054093ac851c60836f9910b645c912ea
DeanYoon <68269605+DeanYoon@users.noreply.github.com>
2023-10-05 17:11:57 +0900

    formatted time

--- src/components/Cards/GuestNote.jsx
+++ src/components/Cards/GuestNote.jsx
@@ -1,20 +1,39 @@
 import styled from "styled-components";
 import { useForm } from "react-hook-form";
 import { Form } from "./Prompt";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
+const requestIp = require("request-ip");
+
+// import formatTimeAgo from "../../utils/dateUtils";
 
 const Wrapper = styled.div`
   font-size: 30px;
+  height: 100%; /* Adjust the maximum height as needed */
 `;
 
-const CommentWrapper = styled.ul``;
-const CommentText = styled.li``;
+const CommentWrapper = styled.div`
+  padding: 0 30px;
+  max-height: 80%;
+  overflow-y: auto;
+`;
+const CommentText = styled.li`
+  list-style-type: none; /* Remove the bullet point */
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+const User = styled.span``;
+const Text = styled.span``;
+const Time = styled.span`
+  font-size: 20px;
+`;
 function GuestNote() {
   const { register, handleSubmit, reset } = useForm();
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
   const [insertedComment, setInsertedComment] = useState();
+  const commentWrapperRef = useRef(null);
   // Function to fetch comments from the server
   const getComments = async () => {
     try {
@@ -51,10 +70,33 @@ function GuestNote() {
     postComment();
   }, [comment]);
 
+  useEffect(() => {
+    if (commentWrapperRef.current) {
+      commentWrapperRef.current.scrollTop =
+        commentWrapperRef.current.scrollHeight;
+    }
+  }, [comments]);
   // Handler for form submission
   const saveText = (data) => {
     setComment(data.comment);
     reset();
+  };
+  const formatTimeAgo = (date) => {
+    const now = new Date();
+    const diff = now.getTime() - date.getTime();
+    if (diff < 1000 * 60) {
+      return `${Math.floor(diff / 1000)}s`;
+    } else if (diff < 1000 * 60 * 60) {
+      return `${Math.floor(diff / (1000 * 60))}m`;
+    } else if (diff < 1000 * 60 * 60 * 24) {
+      return `${Math.floor(diff / (1000 * 60 * 60))}h`;
+    } else if (diff < 1000 * 60 * 60 * 24 * 30) {
+      return `${Math.floor(diff / (1000 * 60 * 60 * 24))}d`;
+    } else {
+      const options = { year: "numeric", month: "2-digit", day: "2-digit" };
+      const formattedDate = new Date(date).toLocaleDateString("ko-KR", options);
+      return formattedDate;
+    }
   };
 
   return (
@@ -62,14 +104,17 @@ function GuestNote() {
       <Form onSubmit={handleSubmit(saveText)}>
         <input
           {...register("comment")}
-          placeholder="Leave comment of feedback"
+          placeholder="Leave comment or feedback"
           required
         />
       </Form>
 
-      <CommentWrapper>
-        {comments.map((comment, index) => (
-          <CommentText key={index}>{comment.text}</CommentText>
+      <CommentWrapper ref={commentWrapperRef}>
+        {comments?.map((comment, index) => (
+          <CommentText key={index}>
+            <Text>{comment.text}</Text>
+            <Time>{formatTimeAgo(new Date(comment.time))}</Time>
+          </CommentText>
         ))}
       </CommentWrapper>
     </Wrapper>
